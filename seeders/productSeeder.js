a3d1e9c97da51ccbdfb099d5be76c98644a863bb
Ivan Paredes <i.a.paredes.rostek@gmail.com>
2023-06-15 09:58:50 -0300

    Datos a seeders

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -106,6 +106,61 @@ module.exports = async () => {
       trending: true,
       price: 99.99,
       slug: slugify("hyperX duocast USB microphone RGB lighting"),
+    },
+    {
+      name: "HyperX Armada 25 FHD Gaming Monitor",
+      image:
+        "https://cdn.shopify.com/s/files/1/0561/8345/5901/products/hyperx_armada_25_fhd_gaming_monitor_g_sync_2_main_no_arm_720x.jpg?v=1678907555",
+      description:
+        "HyperX Armada Gaming Monitors are the first gaming monitors to include the monitor head and ergonomic arm right in the box. Save yourself the hassle of searching for a compatible desk mount, and spend that time dominating the game with the Armada 25’s 240Hz refresh rate and 1ms response time. That rapid responsiveness is built for shooters and competitive games where split-seconds that can tip the balance between victory and getting looted. When you have more time to fully take in the scenery, you’ll have full HD resolution with vibrant colors. Further enhance your gaming experience with NVIDIA® G-SYNC®4, which eliminates screen tearing, stuttering and input lag. Embrace the advantage and space-creating versatility of the HyperX Armada 25 Gaming Monitor and ergonomic arm.",
+      stock: 20,
+      trending: true,
+      price: 349.99,
+      slug: slugify("hyperX armada 25 FHD gaming monitor"),
+    },
+    {
+      name: "HyperX Armada 27 QHD Gaming Monitor",
+      image:
+        "https://cdn.shopify.com/s/files/1/0561/8345/5901/products/hyperx_armada_25_fhd_gaming_monitor_g_sync_2_main_no_arm_720x.jpg?v=1678907555",
+      description:
+        "HyperX Armada Gaming Monitors are the first gaming monitors to include the monitor head and ergonomic arm right in the box. Save yourself the hassle of searching for a compatible desk mount, and spend that time dominating the game with the Armada 25’s 240Hz refresh rate and 1ms response time. That rapid responsiveness is built for shooters and competitive games where split-seconds that can tip the balance between victory and getting looted. When you have more time to fully take in the scenery, you’ll have full HD resolution with vibrant colors. Further enhance your gaming experience with NVIDIA® G-SYNC®4, which eliminates screen tearing, stuttering and input lag. Embrace the advantage and space-creating versatility of the HyperX Armada 25 Gaming Monitor and ergonomic arm.",
+      stock: 20,
+      trending: true,
+      price: 349.99,
+      slug: slugify("hyperX armada 27 FHD gaming monitor"),
+    },
+    {
+      name: "HyperX Pulsefire Haste 2 - Wireless Gaming Mouse",
+      image:
+        "https://cdn.shopify.com/s/files/1/0561/8345/5901/products/hyperx_pulsefire_haste_2_wireless_black_angle_2_720x.jpg?v=1678907837",
+      description:
+        "The HyperX Pulsefire Haste 2 Wireless is built for gamers who can’t afford to waste a millisecond and want the freedom of wireless connectivity. This sequel had a solid upper shell and keeps the 61g weight and 100-hour battery life of the original Pulsefire Haste, but builds upon it with several refinements. Pulsefire Haste 2 Wireless has dual mode wireless connectivity, allowing you to switch between 2.4GHz wireless and Bluetooth connections, and a charging cable so you can charge while using the mouse in wired mode. We’ve also built out a full suite of high-performance HyperX custom components, highlighted by the precision HyperX 26K Sensor with a native DPI of up to 26,000 and a tracking speed of up to 650 IPS. Our custom HyperX Switches provide tactile, audible feedback on each satisfying click, and are rated for a lifespan of up to 100 million clicks. You’ll also enjoy superior physical control of your mouse with smooth, low-friction skates, and the included grip tape. Get the most out of every mouse movement with HyperX NGENUITY Software and create macros for the 6 programmable buttons, customize your DPI settings, and personalize the RGB lighting.",
+      stock: 20,
+      trending: true,
+      price: 89.99,
+      slug: slugify("hyperX pulsefire haste 2 wireless gaming mouse"),
+    },
+    {
+      name: "HyperX Pulsefire Haste - Naruto Edition - Gaming Mouse",
+      image:
+        "https://cdn.shopify.com/s/files/1/0561/8345/5901/products/hyperx_pulsefire_haste_naruto_orange_3_front_angled_720x.jpg?v=1678907529",
+      description:
+        "The HyperX Pulsefire Haste is built for elite gamers looking to gain every fraction of a second possible in their quest to be the best. Weighing in at 59 grams, this full-featured, responsive honeycomb shell mouse has everything you need, just without the extra weight. Boldly declare your ninja way with this limited-edition Naruto-inspired design. TTC Golden micro switches provide satisfying, reliable clicks and are rated for 60 million clicks, so you will not have to worry about missing inputs. The flexible HyperFlex USB cable and the virgin-grade PTFE skates work together to give you a smooth, easy glide to make dominating the game simple.",
+      stock: 20,
+      trending: true,
+      price: 59.99,
+      slug: slugify("hyperX pulsefire haste naruto edition gaming mouse"),
+    },
+    {
+      name: "HyperX Pulsefire Dart - Wireless Gaming Mouse",
+      image:
+        "https://cdn.shopify.com/s/files/1/0561/8345/5901/products/hyperx_pulsefire_dart_01_top_down_720x.jpg?v=1678907870",
+      description:
+        "HyperX Pulsefire Dart™ is a gaming-grade wireless mouse with a reliable 2.4GHz RF connection that will make you wonder why you waited so long to cut the cord. A wireless adapter is included to extend the range for desktop PC setups. It is Qi Certified for wireless charging and can last up to 50 hours on a single charge. With a Pixart 3389 sensor and a split-button design featuring reliable Omron switches, you’ll have all the accuracy you need to crush the competition. It’s ergonomically-shaped and features padded leatherette side grips designed to keep you comfortable during those long nights of gaming. Customize RGB lighting, create macros, manage battery life, and adjust DPI settings with the easy-to-use HyperX NGENUITY Software.",
+      stock: 20,
+      trending: true,
+      price: 99.99,
+      slug: slugify("hyperX pulsefire dart wireless gaming mouse"),
     }
   );
 
