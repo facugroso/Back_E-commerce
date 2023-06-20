58a56b2fd942b7bfb406c77b84c46a0578373e7e
guzmangj <guzmangj96@gmail.com>
2023-06-20 10:57:54 -0300

    Public routes categories

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const userController = require("../controllers/userController");
 const orderController = require("../controllers/orderController");
+const categoryController = require("../controllers/categoryController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
 router.get("/products", productController.index);
 router.get("/products/:id", productController.show);
 router.get("/users/:id", userController.show);
 router.post("/users", userController.store);
+
+router.get("/categories", categoryController.index);
+router.get("/categories/:id", categoryController.show);
 
 router.get(
   "/orders/:id",
