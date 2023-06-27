adb203b83014ba4ce9e61b16e5cc46f851272be0
valentinalr <valentina.lerose@outlook.com>
2023-06-27 17:54:47 -0300

    middleware para ordenes

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-router.use(
-  checkJwt({ secret: process.env.SECRET_ADMIN, algorithms: ["HS256"] })
-);
+function checkUserRole(req, res, next) {
+  checkJwt({ secret: process.env.SECRET_ADMIN, algorithms: ["HS256"] })(
+    req,
+    res,
+    (error) => {
+      if (error) {
+        checkJwt({ secret: process.env.SECRET_USER, algorithms: ["HS256"] })(
+          req,
+          res,
+          (error) => {
+            if (error) {
+              return res.status(401).json({ error: "Unauthorized" });
+            }
+            next();
+          }
+        );
+      } else {
+        next();
+      }
+    }
+  );
+}
 
 router.get("/", orderController.index);
 
