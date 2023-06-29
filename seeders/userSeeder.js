82ee4b5cc8a1466c3a2038566b674c749df8dcbb
valentinalr <valentina.lerose@outlook.com>
2023-06-29 14:31:59 -0300

    test user y nueva password 1234

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -5,12 +5,21 @@ const bcrypt = require("bcryptjs");
 faker.locale = "es";
 
 module.exports = async () => {
+  let encryptedPassword = await bcrypt.hash("1234", 8);
+
   const users = [];
 
+  users.push({
+    firstname: "User",
+    lastname: "Test",
+    email: "usertest@email.com",
+    password: encryptedPassword,
+    phone: faker.phone.phoneNumber(),
+  });
+
   for (let i = 0; i < 8; i++) {
-    let encryptedPassword = await bcrypt.hash("carlos", 8);
     const lastname = faker.name.lastName();
-      const firstname = faker.name.firstName();
+    const firstname = faker.name.firstName();
 
     users.push({
       firstname: firstname,
