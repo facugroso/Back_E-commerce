eb8b4cb3c3cdf1438fd9a2e8fd9f42fb30ea6753
facugroso <110434971+facugroso@users.noreply.github.com>
2023-07-04 16:34:40 -0300

    cambiando data

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,7 +55,7 @@ async function store(req, res) {
       name: fields.name,
       descriptionTitle: fields.descriptionTitle,
       description: fields.description,
-      image: data,
+      image: newFileName,
       //gallery: newGallery,
       features: featureArr,
       stock: fields.stock,
