fcdfa7f59ce9c7deccbf9ee21ea0d518303be45e
facugroso <110434971+facugroso@users.noreply.github.com>
2023-07-04 16:06:26 -0300

    poniendo el half
    estaba bien pero va con comillas

--- controllers/productController.js
+++ controllers/productController.js
@@ -40,6 +40,7 @@ async function store(req, res) {
         cacheControl: "3600",
         upsert: false,
         contentType: files.image.mimetype,
+        duplex: "half",
       });
 
     //const newGallery = [];
